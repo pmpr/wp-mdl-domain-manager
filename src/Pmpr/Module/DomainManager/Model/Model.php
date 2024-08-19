506dfff7093c945f847d313f6c08cd5b7e2acbaa
pmpr.dev <pmpr.dev@gmail.com>
2024-08-19 10:24:47 +0000

    Obfuscated

--- src/Pmpr/Module/DomainManager/Model/Model.php
+++ src/Pmpr/Module/DomainManager/Model/Model.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             66b618610974f             |
+    |             66c31d4b18621             |
     |_______________________________________|
 */
  namespace Pmpr\Module\DomainManager\Model; use Pmpr\Module\DomainManager\Container; class Model extends Container { public function mameiwsayuyquoeq() { Domain::symcgieuakksimmu(); Ownership::symcgieuakksimmu(); } }
--- src/Pmpr/Module/DomainManager/REST/REST.php
+++ src/Pmpr/Module/DomainManager/REST/REST.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             66b618610974f             |
+    |             66c31d4b18621             |
     |_______________________________________|
 */
  namespace Pmpr\Module\DomainManager\REST; use Pmpr\Common\Foundation\REST\RESTRegister; class REST extends RESTRegister { public function gigwcakmiyayoigw() { $this->ogyceaekywowkqsc(Controller::symcgieuakksimmu()); } }
